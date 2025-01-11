72f9db7399e5a93445a26e5bf0a4c82b91221670
flater1c <92336292+flater1c@users.noreply.github.com>
2025-01-11 20:11:53 +0100

    help addition

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,7 @@ const program = new Command();
 
 program
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
+    .version('1.0.0')
+    .option('-f, --format <type>', 'output format');
 
 program.parse();
