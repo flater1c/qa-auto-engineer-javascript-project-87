42d909d25e019dd0d238608ad3ebf439aeaf1dea
flater1c <92336292+flater1c@users.noreply.github.com>
2025-01-22 22:06:01 +0100

    default format fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,7 @@ import genDiff from '../src/gendiff.js';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format', 'diff')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .argument('<file1>', 'first file')
   .argument('<file2>', 'second file')
   .action((file1, file2, options) => {
