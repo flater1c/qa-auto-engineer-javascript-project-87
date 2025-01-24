90d268181d759b84d1c5592570b5ab1abdab2302
flater1c <92336292+flater1c@users.noreply.github.com>
2025-01-24 20:12:04 +0100

    parsers refactored

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,37 +1,39 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
-import parseJSONFiles, { parseYMLFiles } from '../src/parse.js';
+import parseFile from '../src/parse.js';
 import diffOutput from '../formatters/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const parsedJsons = parseJSONFiles(getFixturePath('file1.json'), getFixturePath('file2.json'));
-const parsedYamls = parseYMLFiles(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
+const parsedJson1 = parseFile(getFixturePath('file1.json'), 'json');
+const parsedJson2 = parseFile(getFixturePath('file2.json'), 'json');
+const parsedYaml1 = parseFile(getFixturePath('file1.yml'), 'yaml');
+const parsedYaml2 = parseFile(getFixturePath('file2.yml'), 'yaml');
 const plainOutput = fs.readFileSync(getFixturePath('plain_output.txt'), 'utf-8');
 const stylishOutput = fs.readFileSync(getFixturePath('stylish_output.txt'), 'utf-8');
 const JSONOutput = fs.readFileSync(getFixturePath('json_output.json'), 'utf-8');
 
 test('Checking flat jsons', () => {
-  expect(diffOutput(parsedJsons.output1, parsedJsons.output2, 'stylish')).toBe(stylishOutput);
+  expect(diffOutput(parsedJson1, parsedJson2, 'stylish')).toBe(stylishOutput);
 });
 test('Checking flat yamls', () => {
-  expect(diffOutput(parsedYamls.output1, parsedYamls.output2, 'stylish')).toBe(stylishOutput);
+  expect(diffOutput(parsedYaml1, parsedYaml2, 'stylish')).toBe(stylishOutput);
 });
 test('Checking plain output', () => {
-  expect(diffOutput(parsedYamls.output1, parsedYamls.output2, 'plain')).toBe(plainOutput);
+  expect(diffOutput(parsedYaml1, parsedYaml2, 'plain')).toBe(plainOutput);
 });
 test('Checking json output', () => {
-  expect(diffOutput(parsedYamls.output1, parsedYamls.output2, 'json')).toBe(JSONOutput);
+  expect(diffOutput(parsedYaml1, parsedYaml2, 'json')).toBe(JSONOutput);
 });
 test('Checking wrong output format type', () => {
   expect(() => {
-    diffOutput(parsedYamls.output1, parsedYamls.output2, 'anytype');
+    diffOutput(parsedYaml1, parsedYaml2, 'anytype');
   }).toThrow(Error);
 });
 test('Checking invalid files parsing', () => {
-  expect(parseJSONFiles(`${parsedJsons.output1}1`, `${parsedJsons.output2}1`)).toBeNull();
-  expect(parseYMLFiles(`${parsedYamls.output1}1`, `${parsedYamls.output2}1`)).toBeNull();
+  expect(parseFile(parsedJson1 + '1', 'json')).toBeNull();
+  expect(parseFile(parsedYaml1 + '1', 'yaml')).toBeNull();
 });
--- src/parse.js
+++ src/parse.js
@@ -1,34 +1,16 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
 
-const parseJSONFiles = (file1, file2) => {
+export default (file, fileFormat) => {
   try {
-    const data1 = fs.readFileSync(file1, 'utf8');
-    const output1 = JSON.parse(data1);
-
-    const data2 = fs.readFileSync(file2, 'utf8');
-    const output2 = JSON.parse(data2);
-
-    return { output1, output2 };
+    if (fileFormat === 'json') {
+      return JSON.parse(fs.readFileSync(file, 'utf8'));
+    }
+    if (fileFormat === 'yaml') {
+      return yaml.load(fs.readFileSync(file, 'utf8'));
+    }
   } catch (error) {
     console.error('Ошибка при обработке файлов:', error);
     return null;
   }
 };
-
-export const parseYMLFiles = (file1, file2) => {
-  try {
-    const data1 = fs.readFileSync(file1, 'utf8');
-    const output1 = yaml.load(data1);
-
-    const data2 = fs.readFileSync(file2, 'utf8');
-    const output2 = yaml.load(data2);
-
-    return { output1, output2 };
-  } catch (error) {
-    console.error('Ошибка при обработке файлов:', error);
-    return null;
-  }
-};
-
-export default parseJSONFiles;
