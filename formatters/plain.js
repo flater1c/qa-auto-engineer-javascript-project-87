721e2a7b67691fca34f6fb39889bc19ac2528d74
flater1c <92336292+flater1c@users.noreply.github.com>
2025-01-24 17:45:38 +0100

    formatters refactor

--- formatters/plain.js
+++ formatters/plain.js
@@ -3,8 +3,10 @@ import _ from 'lodash';
 export default (file1, file2) => {
   const keys1 = Object.keys(file1);
   const keys2 = Object.keys(file2);
-  const uniqueKeys = _.union(keys1, keys2).sort();
-  const result = uniqueKeys.reduce((acc, key) => {
+  const uniqueKeys = _.union(keys1, keys2);
+  const sortedUniqueKeys = _.orderBy(uniqueKeys, [], ['asc']);
+
+  const result = sortedUniqueKeys.reduce((acc, key) => {
     if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
       return `${acc}Property '${key}' was removed\n`;
     }
