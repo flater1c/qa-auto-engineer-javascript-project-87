dbf86052c11a2104f9575c4341bcf15b7f076160
flater1c <92336292+flater1c@users.noreply.github.com>
2025-01-24 16:51:18 +0100

    lint fix

--- formatters/diff.js
+++ formatters/diff.js
@@ -3,9 +3,10 @@ import _ from 'lodash';
 export default (file1, file2) => {
   const keys1 = Object.keys(file1);
   const keys2 = Object.keys(file2);
-  const uniqueKeys = _.union(keys1, keys2).toSorted();
+  const uniqueKeys = _.union(keys1, keys2);
+  const sortedUniqueKeys = _.orderBy(uniqueKeys, [], ['asc']);
 
-  const result = uniqueKeys.reduce((acc, key) => {
+  const result = sortedUniqueKeys.reduce((acc, key) => {
     if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
       return `${acc}  - ${key}: ${_.get(file1, key)}\n`;
     }
@@ -15,7 +16,7 @@ export default (file1, file2) => {
     if (file1[key] === file2[key]) {
       return `${acc}    ${key}: ${file1[key]}\n`;
     }
-    return `${acc}  - ${key}: ${file1[key]}\n` + `  + ${key}: ${file2[key]}\n`;
+    return `${acc}  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}\n`;
   }, '{\n');
   return `${result.trim()}\n}`;
 };
